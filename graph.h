852ed60bac705d10f0e55c8f7efcdb0d5ebbf209
Carl Denton <carldenton@college.harvard.edu>
2017-02-25 18:20:38 -0500

    header file done

--- graph.h
+++ graph.h
@@ -3,19 +3,53 @@
  * Carl Denton and Olivia Chung
  */
 
-// edge struct
-typedef struct edge {
-    int source;
-    int dest;
-    float weight;
+/* vertex struct */
+struct vertex {
+    /* which kind of graph is this? */
+    int type; 
+
+    /* 
+     * NULL if we are the root 
+     * otherwise points to some other node
+     */
+    struct vertex *set;
+
+    /* linked list for storage */
+    struct vertex *next_vert;
+
+    /* coordinates for parts 2 and 3 */
+    int x; 
+    int y;
+    int z;
+    int a;
 };
 
+/* edge struct */
+typedef struct edge {
+    struct edge *next_edge;
+    struct vertex *source;
+    struct vertex *dest;
+    double weight;
+};
+
+/* graph struct */
 typedef struct graph {
     /* number of nodes in this graph */
-    int n;
+    long n;
 
-    struct edge* head;
+    /* point to our edge list */
+    struct edge *head;
 };
 
+/* generate a graph */
+struct graph *generate(long n);
 
-void insert(struct graph*, edge *e, int source, int dest, float weight);
+/* find the weight of the minimum spanning tree on a graph */
+float mst_weight(struct graph *g);
+
+/* set operations for kruskal's algorithm */
+void set_union(struct vertex *v1, struct vertex *v2);
+void set_find(struct vertex *v);
+
+/* sorting routine for kruskal's algorithm */
+struct edge *sort_by_weight(struct edge *head);
